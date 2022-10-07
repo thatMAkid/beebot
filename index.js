14223666b0c466b052a47ac5d0fa975202dccf30
thatMAkid <morgan_a@beezwax.net>
2022-10-07 09:52:43 -0700

    First Commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// const { App } = require('@slack/bolt');
+
+// const bot = new SlackBot({
+//     token: 'xoxb-3147039373-4199354300033-jGTWu7qnpo543MAAT9dJjWkW',
+//     name: 'beebot'
+// });
+
+// // Start Handler
+// bot.on('start', ()=> {
+//     const params = {
+//         icon_emoji: ':smiley:'
+//     }
+
+//     bot.postMessageToChannel('fm-interns-july-2022', 'We Testin\' Over Here!', params);
+//     bot.postMessageToUser('morgan_a', 'meow!', params);
+// });
+
+// // Error Handler
+// bot.on('error', (err) => console.log(err));
+
+// // Message Handler
+// bot.on('message', (data) => {
+
+// });
